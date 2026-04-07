9adb20fba6281cf6cf328857cc0c5e0868d87f0a
Emanuel <ekelle21@jh.edu>
2026-04-06 21:15:09 -0400

    Update the main.py

--- Project_main.py
+++ Project_main.py
@@ -1,10 +1,12 @@
 import Project_env
 import Project_agent
 
+import time
+
 import numpy as np
 
 if __name__ == "__main__":
-    env = Project_env.BoardGame(print_states=False)
+    env = Project_env.BoardGame(n=4, print_states=False)
     agent = Project_agent.QLearner()
 
     agent.set_epsilon(0.1)
@@ -12,6 +14,9 @@ if __name__ == "__main__":
     agent.set_gamma(1.0)
 
     num_episodes = 100_000
+    start_time = time.time()
     agent.play_episodes(env, num_episodes)
+    finish_time = time.time()
     print(f"Average returns: {np.mean(agent.returns)}")
     print(f"Number of visited states: {len(agent.q_table.keys())}")
+    print(f"Total time in minutes: {(finish_time - start_time)/60}")
