91b8673dbf94e20b099ff4b272f6e264e637b0a0
Emanuel <ekelle21@jh.edu>
2026-04-10 23:13:42 -0400

    Continue with upgrades

--- Project_env.py
+++ Project_env.py
@@ -8,108 +8,30 @@ class BoardGame:
         self.print_states = print_states
         self.n = n
 
-        self.p1_start = 3
-        self.p1_end = 2
         self.rosettes = [3, 7, 13]
         self.p2_start = 19
         self.p2_end = 18
 
-        self.p1_pieces = n
-        self.p2_pieces = n
+        self.p1_pieces = self.n
+        self.p2_pieces = self.n
 
         self.p1_states = np.array([7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
         self.p2_states = np.array([7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
 
+    def encode_state(self, ai, bi, roll):
+        a = ai[1:]
+        b = bi[1:]
 
-        self.start_board = np.array([
-                [0, 0, 0, self.n, 0, 0, 0, 0],
-                [0, 0, 0,      0, 0, 0, 0, 0],
-                [0, 0, 0, self.n, 0, 0, 0, 0]
-        ])
+        a_bits = "".join(map(str, a))
+        b_bits = "".join(map(str, b))
 
-        # self.rosettes = {0: (1, 7), 1: (4, ), 2: (1, 7)}
-        self.rosettes = {(0, 7), (0, 1), (1, 4), (2, 7), (2, 1)}
-        self.rosettes_lane = {4, 8, 14}
-        self.grid, self.inverse_grid = self.create_grid()
-        self.rolls = []
+        state_bits = a_bits + b_bits
 
-    def encode_state(self, turn, board, roll):
-        rolls = np.binary_repr(roll)
-        turn = int(turn)
-        p1 = board[0, 2]
-        p2 = board[2, 2]
-        p1_blue = ''.join(board[0, [4, 5, 6, 7, 0, 1]].astype('<U1'))
-        p2_blue = ''.join(board[2, [4, 5, 6, 7, 0, 1]].astype('<U1'))
+        roll_bits = f"{roll:03b}"
 
-        green = ''.join(board[1].astype('<U1'))
-
-        green_bin = np.binary_repr(int(green, 3))
-        p1_bin = np.binary_repr(p1)
-        p2_bin = np.binary_repr(p2)
-
-        p1_bin = '0'*(3-len(p1_bin)) + p1_bin
-        p2_bin = '0'*(3-len(p2_bin)) + p2_bin
-        green_bin = '0'*(13-len(green_bin)) + green_bin
-
-        bit_string = f"{int(turn)}{rolls}{p1_bin}{p2_bin}{p1_blue}{p2_blue.replace('2','1')}{green_bin}"
-        # print(f'binary: {bit_string}')
-        # assert len(bit_string) == 37, f'{len(bit_string)} {bit_string}'
+        bit_string = roll_bits + state_bits
 
         return int(bit_string, 2)
-
-    def decode_state(self, state):
-        # print("DECODE")
-        bits = np.binary_repr(state)
-        bits = '0'*(32-len(bits)) + bits
-
-        turn = int(bits[0])
-        p1_score = int(bits[1:3], 2)
-        p2_score = int(bits[3:6], 2)
-        p1_blue = bits[7:13]
-        p2_blue = bits[13:19].replace('1', '2')
-        green = np.base_repr(int(bits[19:], 2), 3)
-        green = '0'*(8-len(green)) + green
-
-        board = np.array([
-            [*list(p1_blue[:2]), p1_score, self.n - p1_score, *list(p1_blue[2:])],
-            list(green),
-            [*list(p2_blue[:2]), p2_score, self.n - p2_score, *list(p2_blue[2:])]
-        ]).astype(int)
-
-        scores = {
-            1: p1_score,
-            2: p2_score
-        }
-        for player, score in scores.items():
-            idx = player if player == 2 else 0
-            lane_mask = [(idx, 4), (idx, 5), (idx, 6), (idx, 7),
-                    (1, 7), (1, 6), (1, 5), (1, 4), (1, 3), (1, 2), (1, 1), (1, 0),
-                    (idx, 0), (idx, 1)]
-            
-            lane = np.array([board[x] for x in lane_mask])
-            board[idx, 3] = self.n - scores[player] - (lane == player).sum()
-        # print(board)
-
-        return turn, board
-
-    def create_grid(self):
-        grid = {}
-        grid_inv = {}
-        i = 0
-        for j in range(self.start_board.shape[1]):
-            grid[i] = (0, j)
-            grid_inv[(0, j)] = i
-            i += 1
-        for j in reversed(range(self.start_board.shape[1])):
-            grid[i] = (1, j)
-            grid_inv[(1, j)] = i
-            i += 1
-        for j in range(self.start_board.shape[1]):
-            grid[i] = (2, j)
-            grid_inv[(2, j)] = i
-            i += 1
-
-        return grid, grid_inv
 
     def execute_action(self, previous_state, action, player_turn=0, roll=np.random.choice([0, 1, 2, 3, 4], p=[1/16, 1/4, 3/8, 1/4, 1/16])):
         """
@@ -182,64 +104,34 @@ class BoardGame:
 
         return a_indices
 
-    def transition(self, previous_state, action, roll, player_turn=0):
+    def transition(self, ai, bi, action, roll):
         """
         Performs the transition to the next state
         """
+        if action + roll == 15:
+            ai[action] = 0
+            return ai, bi
 
-        current_row, current_column = self.grid[action]
+        ai[action] = 0
+        ai[action + roll] = 1
 
-        if player_turn == 0:
-            if action + roll > 15:
-                next_square_index = action + roll - 16
-            else:
-                next_square_index = action + roll
-        else:
-            if action + roll > 23:
-                next_square_index = action + roll - 16
-            else:
-                next_square_index = action + roll
+        if 4 < action + roll <= 12:
+            if bi[action + roll] == 1:
+                bi[0] += 1
+                bi[action + roll] = 0
 
-        next_row, next_column = self.grid[next_square_index]
+        return ai, bi
 
-        if player_turn == 0:
-            if previous_state[next_row, next_column] == 2 and next_square_index != self.p1_end:
-                previous_state[2, 3] += 1
-            if next_square_index != self.p1_end:
-                previous_state[next_row, next_column] = 1
-        else:
-            if previous_state[next_row, next_column] == 1 and next_square_index != self.p2_end:
-                previous_state[0, 3] += 1
-            if next_square_index != self.p2_end:
-                previous_state[next_row, next_column] = 2
-
-        if action == self.p1_start or action == self.p2_start:
-            previous_state[current_row, current_column] -= 1
-        elif next_square_index == self.p1_end or next_square_index == self.p2_end:
-            previous_state[next_row, next_column] += 1
-            previous_state[current_row, current_column] = 0
-        else:
-            previous_state[current_row, current_column] = 0
-
-        next_turn = player_turn
-        if (next_row, next_column) not in self.rosettes: 
-            next_turn = int(not player_turn) 
-        else:
-            if player_turn == 1:
-                self.p2_rosette = True
-
-        return next_turn, previous_state
-
-    def get_terminal_flag(self, board):
+    def get_terminal_flag(self, ai, bi):
         """
         Returns true if the current state is a terminal state, and false otherwise
         """
-        if board[0, 2] == self.n or board[2, 2] == self.n:
-            return True
-        else:
-            return False
+        return ai.sum() == self.n or bi.sum() == self.n
 
     def reset(self):
-        self.start_board[:] = 0.0
-        self.start_board[0, 3] = self.n
-        self.start_board[2, 3] = self.n
+        self.p1_states[:] = 0.0
+        self.p2_states[:] = 0.0
+
+        self.p1_states[0] = self.n
+        self.p2_states[0] = self.n
+
