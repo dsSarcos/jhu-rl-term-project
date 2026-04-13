908bc05ac250b01a02d3ac8dcfb6d7545e427fbb
Emanuel <ekelle21@jh.edu>
2026-04-13 10:27:15 -0400

    Updait main.py

--- Project_main.py
+++ Project_main.py
@@ -6,12 +6,23 @@ import time
 import numpy as np
 
 
-def training_agent(learning_file_name, return_file_name, env, num_episodes, eps, alpha, eps_min=-1, n_dec=-1, agent_type="Q-Learning"):
+def training_agent(learning_file_name,
+                   return_file_name,
+                   env,
+                   num_episodes,
+                   eps,
+                   alpha,
+                   eps_min=-1.0,
+                   n_dec=-1.0,
+                   agent_type="Q-Learning",
+                   retrain=False):
     agent = None
     if agent_type == "Q-Learning":
         agent = Project_agent.QLearner(file_name=learning_file_name, n_dec=n_dec, eps_min=eps_min)
     agent.set_epsilon(eps)
     agent.set_alpha(alpha)
+    if retrain is True:
+        agent.load_learning_table()
     agent.play_episodes(env, num_episodes)
     agent.save_learning_table()
     np.savetxt(return_file_name, agent.returns, delimiter=',')
@@ -38,17 +49,18 @@ def run_agent(learning_file_name, return_file_name, env, num_episodes, eps, alph
 if __name__ == "__main__":
     before_training = False
     training = True
+    testing = False
 
-    learning_file_name = 'q_learner.csv'
     num_episodes = 500_000
     n_dec = 200_000
-    eps = 0.7
-    eps_min = 0.1
+    eps = 0.1
+    eps_min = -1.0
     alpha = 0.1
     env = Project_env.BoardGame(n=4, print_states=False)
 
     if before_training is True:
         return_file_name = 'Project_experiment_before_training.csv'
+        learning_file_name = None
         num_episodes = 1000
         start_time = time.time()
         agent = run_agent(learning_file_name,
@@ -62,8 +74,9 @@ if __name__ == "__main__":
         print(f"Number of more games won than environment: {sum(agent.returns)}")
         print(f"Total time in minutes: {(finish_time - start_time)/60}")
 
-    if training is True:
-        return_file_name = 'Project_experiment_train.csv'
+    elif training is True:
+        return_file_name = 'Project_experiment_retrained.csv'
+        learning_file_name = 'q_learner.csv'
         start_time = time.time()
         agent = training_agent(learning_file_name,
                                return_file_name,
@@ -73,14 +86,16 @@ if __name__ == "__main__":
                                alpha,
                                eps_min=eps_min,
                                n_dec=n_dec,
-                               agent_type="Q-Learning")
+                               agent_type="Q-Learning",
+                               retrain=True)
         finish_time = time.time()
         print(f"Average training returns: {np.mean(agent.returns)}")
         print(f"Number of visited states: {len(agent.q_table.keys())}")
         print(f"Total time in minutes: {(finish_time - start_time)/60}")
 
-    else:
+    elif testing is True:
         return_file_name = 'Project_experiment_test.csv'
+        learning_file_name = None
         num_episodes = 1000
         start_time = time.time()
         agent = run_agent(learning_file_name,
